1c82b66149ba61b05fd11c942f532a0b971e38e2
Matthew Masiello <sotek222@yahoo.com>
2020-04-23 12:25:32 -0400

    adds initial configuration for sequelize

--- Module-9_Sequelize/node-server/util/db.js
+++ Module-9_Sequelize/node-server/util/db.js
@@ -1,10 +1,22 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'node-complete',
-  password: 'lizardmen'
-});
+const sequelize = new Sequelize('node-complete', 'root', 'lizardmen', {
+    dialect: 'mysql',
+    host: 'localhost'
+  });
 
-module.exports = pool.promise();
+module.exports = sequelize;
+
+
+// ---------- MYSQL without Sequelize SETUP ------------- // 
+
+// const mysql = require('mysql2');
+
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   database: 'node-complete',
+//   password: 'lizardmen'
+// });
+
+// module.exports = pool.promise();
