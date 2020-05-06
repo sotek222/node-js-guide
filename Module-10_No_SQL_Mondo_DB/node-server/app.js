6242fc760c012a8d547c5dc588b17c7ddc0d2b12
Matthew Masiello <sotek222@yahoo.com>
2020-05-06 13:57:56 -0400

    removes sequelize funcitonality

--- Module-10_No_SQL_Mondo_DB/node-server/app.js
+++ Module-10_No_SQL_Mondo_DB/node-server/app.js
@@ -1,17 +1,6 @@
 // environment variables
 require('dotenv').config();
 
-// Database
-const sequelize = require('./util/db');
-
-// Models
-const User = require('./models/user');
-const Product = require('./models/product');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cartItem');
-const Order = require('./models/order');
-const OrderItem = require('./models/orderItem');
-
 // Node Core Modules:
 const path = require('path');
 
@@ -35,55 +24,19 @@ app.use(express.static(path.join(rootDir, 'public')));
 // Parses all incoming requests with a body, and returns a parsed request.
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, resp, next) => {
-  User.findByPk(1)
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.error("ERROR: ", err));
-});
+// app.use((req, resp, next) => {
+//   User.findByPk(1)
+//     .then(user => {
+//       req.user = user;
+//       next();
+//     })
+//     .catch(err => console.error("ERROR: ", err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(resourceNotFound);
 
-
-// Associations:
-// Sets up the association between Products and Users
-// onDelete tells Sequelize that when a User is deleted
-// also delete the associated Products
-
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-User.hasMany(Product);
-User.hasOne(Cart);
-User.hasMany(Order);
-Order.belongsTo(User);
-Order.belongsToMany(Product, { through: OrderItem });
-Product.belongsToMany(Order, { through: OrderItem });
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-
-
-// This look at any models in the program
-// that use the define method and checks to see if the 
-// corresponding table exists and if it doesnt creates it
-// force: true drops tables everytime and rebuilds them 
-// but should not be used in prodution
-sequelize
-// .sync({ force: true })
-  .sync()
-  .then(result => User.findByPk(1))
-  .then(user => {
-    if(!user){
-      return User.create({name: "Matt", email: "matt@test.com"})
-        .then(user => user.createCart())
-    };
-    return user;
-  })
-  .then(cart => app.listen(3000))
-  .catch(err => console.error("ERROR IN App.js: ", err));
 
 
